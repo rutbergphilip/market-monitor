d9b2855cad74accc8aa58c51c4c33a1cdbed477a
Philip Rutberg <54883264+rutbergphilip@users.noreply.github.com>
2025-03-14 10:30:04 +0100

    feat: set up cron job with dotenv configuration and blocket integration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+import 'tsconfig-paths/register';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { CronJob } from 'cron';
+
+import { blocketJob } from '@/integrations/cron/blocket-job';
+
+const job = CronJob.from({
+  cronTime: process.env.AD_CRON_TIME || '*/5 * * * *',
+  onTick: blocketJob,
+  start: true,
+  timeZone: process.env.BLOCKET_TIMEZONE || 'Europe/Stockholm',
+  runOnInit: true,
+});
+
+job.start();
