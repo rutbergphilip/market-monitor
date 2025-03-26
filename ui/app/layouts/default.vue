9449f61ddc0e1361a0464f4c9c273c29abc3af5a
Philip Rutberg <54883264+rutbergphilip@users.noreply.github.com>
2025-03-26 21:30:33 +0100

    feat: integrated Sidebar component into the default layout for improved structure

--- ui/app/layouts/default.vue
+++ ui/app/layouts/default.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
-    <slot />
+  <div class="flex">
+    <Sidebar class="flex-shrink-0" />
+    <div class="flex-1">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+        <slot />
+      </div>
+    </div>
   </div>
 </template>
