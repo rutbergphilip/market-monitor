fdec2005019a6cbf5b4d849360bcd3d0f60022fa
Philip Rutberg <54883264+rutbergphilip@users.noreply.github.com>
2025-03-27 21:35:25 +0100

    feat: enhanced Sidebar component with toggle functionality and improved tooltip interactions

--- ui/app/components/Sidebar.vue
+++ ui/app/components/Sidebar.vue
@@ -124,23 +124,70 @@ const items = ref<NavigationMenuItem[][]>([
     },
   ],
 ]);
+
+const isCollapsed = ref(false);
+
+function toggleSidebar() {
+  isCollapsed.value = !isCollapsed.value;
+}
 </script>
 
 <template>
   <UContainer
     class="flex flex-col h-screen py-5 gap-5 bg-neutral-800 border-r-[1px] border-r-gray-100 dark:border-r-gray-700"
+    :class="{ 'w-16': isCollapsed }"
   >
-    <header>
-      <h1 class="font-bold text-xl">Blocket Bot</h1>
+    <header
+      class="flex items-center justify-between"
+      :class="{ 'justify-center': isCollapsed }"
+    >
+      <h1 v-if="!isCollapsed" class="font-bold text-xl">Blocket Bot</h1>
+      <UTooltip
+        v-if="isCollapsed"
+        text="Expand sidebar"
+        :delay-duration="250"
+        :content="{
+          side: 'right',
+        }"
+      >
+        <UButton
+          :ui="{
+            leadingIcon: 'scale-125',
+          }"
+          size="xl"
+          variant="ghost"
+          icon="tabler:layout-sidebar-left-expand"
+          @click="toggleSidebar"
+        />
+      </UTooltip>
+      <UTooltip
+        v-else
+        text="Collapse sidebar"
+        :delay-duration="250"
+        :content="{
+          side: 'bottom',
+        }"
+      >
+        <UButton
+          :ui="{
+            leadingIcon: 'scale-125',
+          }"
+          size="xl"
+          variant="ghost"
+          icon="tabler:layout-sidebar-left-collapse"
+          @click="toggleSidebar"
+        />
+      </UTooltip>
     </header>
 
     <UNavigationMenu
+      v-if="!isCollapsed"
       orientation="vertical"
       :items="items"
       class="data-[orientation=vertical]:w-48"
     />
 
-    <footer class="mt-auto">
+    <footer v-if="!isCollapsed" class="mt-auto">
       <p>Signed in</p>
     </footer>
   </UContainer>
