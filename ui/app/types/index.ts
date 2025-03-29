4e9ad820c860bd89941fea7bcdc84024133f5cf4
Philip Rutberg <54883264+rutbergphilip@users.noreply.github.com>
2025-03-29 23:08:01 +0100

    fix: made Watcher properties optional to improve flexibility in data handling

--- ui/app/types/index.ts
+++ ui/app/types/index.ts
@@ -1,11 +1,11 @@
 export type NotificationTarget = 'DISCORD' | 'EMAIL' | 'SLACK';
 
 export type Watcher = {
-  id: string;
-  status: 'active' | 'paused';
+  id?: string;
+  status?: 'active' | 'paused';
   query: string;
-  number_of_runs: number;
-  last_run: string;
+  number_of_runs?: number;
+  last_run?: string;
   notifications: NotificationTarget[];
   schedule: string;
   created_at?: string;
