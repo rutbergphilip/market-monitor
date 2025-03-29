20c53e192ee5bfa8bb489be716095f5e6d03fc6b
Philip Rutberg <54883264+rutbergphilip@users.noreply.github.com>
2025-03-29 16:47:32 +0100

    feat: updated index page layout to include Watchers section with management options

--- ui/app/pages/index.vue
+++ ui/app/pages/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>Blocket Bot UI</div>
+  <div class="flex flex-col gap-4">
+    <div class="flex justify-between items-center">
+      <div class="flex flex-col gap-2">
+        <h1 class="text-3xl font-bold">Watchers</h1>
+        <p class="text-lg">Manage your watchers.</p>
+      </div>
+
+      <UButton class="self-end" leading-icon="material-symbols:add-2" size="lg">
+        Add Watcher
+      </UButton>
+    </div>
+
+    <USeparator />
+
+    <div class="flex flex-col gap-4">
+      <WatcherTable />
+    </div>
+  </div>
 </template>
