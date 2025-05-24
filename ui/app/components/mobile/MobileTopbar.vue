6ce931e5528ecbca0f006f5bbc6c1563475006d1
Philip Rutberg <54883264+rutbergphilip@users.noreply.github.com>
2025-05-24 14:01:27 +0200

    fix(MobileTopbar): updated dark background color to neutral-900 for consistency

--- ui/app/components/mobile/MobileTopbar.vue
+++ ui/app/components/mobile/MobileTopbar.vue
@@ -3,7 +3,7 @@ const statesStore = useStatesStore();
 </script>
 
 <template>
-  <div class="bg-white dark:bg-gray-900 shadow h-16 flex items-center px-4">
+  <div class="bg-white dark:bg-neutral-900 shadow h-16 flex items-center px-4">
     <UButton
       icon="i-heroicons-bars-3"
       variant="ghost"
